56f6332a90447527100aeca19953e8ebecf62e02
Cory <Coryf65@gmail.com>
2018-06-26 21:47:01 -0500

    Changed text into VARCHAR(50), makes comparing easier

--- SQLBasics/SQLQuerys.sql
+++ SQLBasics/SQLQuerys.sql
@@ -1,5 +1,12 @@
-﻿SELECT * FROM books
+﻿-- Seeing the All of the tables Data
 
-SELECT * FROM loans
+--SELECT * FROM books
 
-SELECT * FROM patrons
+--SELECT * FROM loans
+
+---SELECT * FROM patrons
+
+-- Question 1 --
+
+-- What books in our library were authored by J.K. Rowling before the year 2000?
+SELECT title FROM books WHERE author = 'J.K. Rowling' AND first_published < 2000;
