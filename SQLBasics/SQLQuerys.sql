37910315ee8d4e6ea745caa652331aa5b739194f
Cory <Coryf65@gmail.com>
2018-06-26 21:49:53 -0500

    Showing the differences between the AND / OR Keywords

--- SQLBasics/SQLQuerys.sql
+++ SQLBasics/SQLQuerys.sql
@@ -10,3 +10,9 @@
 
 -- What books in our library were authored by J.K. Rowling before the year 2000?
 SELECT title FROM books WHERE author = 'J.K. Rowling' AND first_published < 2000;
+
+-- if we used OR in replace it can give us very different results
+SELECT title FROM books WHERE author = 'J.K. Rowling' OR first_published < 2000;
+
+-- What books do we have in the library authored by "Ernest Cline" or "Andy Weir"?
+SELECT title FROM books WHERE author = "Ernest Cline" OR author = "Andy Weir";    
