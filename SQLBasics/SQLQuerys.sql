100ccfec7cb483447057c3d8367c3e947f51fb76
Cory <Coryf65@gmail.com>
2018-06-27 17:21:24 -0500

    Added some more notes/comments

--- SQLBasics/SQLQuerys.sql
+++ SQLBasics/SQLQuerys.sql
@@ -1,15 +1,12 @@
-﻿-- Seeing the All of the tables Data
+﻿-- Questions --
 
---SELECT * FROM books
-
---SELECT * FROM loans
-
----SELECT * FROM patrons
-
--- Question 1 --
+-- Both condition 1 AND condition 2 Must match
 
 -- What books in our library were authored by J.K. Rowling before the year 2000?
 SELECT title FROM books WHERE author = 'J.K. Rowling' AND first_published < 2000;
+
+-- NOTES:
+-- Either condition 1 OR condition 2 Must match
 
 -- if we used OR in replace it can give us very different results
 SELECT title FROM books WHERE author = 'J.K. Rowling' OR first_published < 2000;
@@ -26,12 +23,18 @@ SELECT * FROM loans WHERE return_by > '2015-12-18';
 -- Which users have the id of 1 or 3?
 SELECT first_name, email FROM patrons WHERE id = 1 OR id = 3;
 
+-- NOTES:
+-- We can replace a search of OR's with an IN Keyword
 
 -- SELECT <columns> FROM <table> WHERE <condition 1> OR <condition 2> OR <condition 3>;
 SELECT first_name, email FROM patrons WHERE library_id = "MCL1001" OR library_id = "MCL1100" OR library_id = "MCL1011";
 
--- simplified  version of line 31
+-- simplified  version of the last query
 SELECT first_name, email FROM patrons WHERE library_id IN ("MCL1001", "MCL1100", "MCL1011");
 
 -- OR look for something not inside the values
 SELECT first_name, email FROM patrons WHERE library_id NOT IN ("MCL1001", "MCL1100", "MCL1011");
+
+
+--- What are the book in the library from the 19th century?
+SELECT title, author FROM books WHERE first_published >= 1800 AND first_published <= 1899;
