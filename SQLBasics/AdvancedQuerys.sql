9704c3ebacc5c5d3031cb9abf2c1950d1f698fed
Cory <Coryf65@gmail.com>
2018-06-30 20:39:51 -0500

    Left Oter Joins

--- SQLBasics/AdvancedQuerys.sql
+++ SQLBasics/AdvancedQuerys.sql
@@ -9,3 +9,10 @@
 		INNER JOIN model AS md ON mk.MakeID = md.MakeID
 		WHERE MakeName = 'Chevy';
 
+-- SELECT <columns> FROM <table1>  LEFT OUTER JOIN <table2> ON <equality criteria> WHERE <search criteria> ...
+
+-- How many models of each make are there?
+
+SELECT mk.MakeName, COUNT(md.ModelName) AS NumberofModels FROM make AS mk 
+  LEFT OUTER JOIN model AS md ON mk.MakeID = md.MakeID
+  GROUP BY mk.MakeName; 
